e0558b06c4d869c8a6d49a28cc3548a401be73df
Saenro Gonsalves <aenros09602@gmail.com>
2024-02-08 08:55:00 +0530

    first master commit

---
+++ Case/src/ex/Movie.java
@@ -0,0 +1,14 @@
+package ex;
+
+public class Movie {
+	private String title;
+	
+private String director;
+public Movie(String title,String director) {
+	this.title=title;
+	this.director=director;
+}
+public String getTittle() {
+	return this.title;
+}
+}
